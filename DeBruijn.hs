b4187711dafb7a4b28a41d7a9f6dc79a58a80151
W <jonathan.wnog@gmail.com>
2024-12-06 21:29:03 -0300

    names

--- DeBruijn.hs
+++ DeBruijn.hs
@@ -1,8 +1,8 @@
 module DeBruijn where
 
-import Data.List (elemIndex, (\\))
-import Lambda (LTerm (evalStep))
-import qualified Lambda as Lam (Term (), freeVars)
+import Data.List ( elemIndex, (\\) )
+import Lambda ( LTerm ( evalStep ) )
+import qualified Lambda as Lam ( Term ( Var, Abs, App ), freeVars )
 
 type Gamma = [Char]
 
@@ -24,4 +24,18 @@ isValue (Abs _) = True
 isValue _ = False   -- aplication isnt value
 
 removeNames :: Lam.Term -> Gamma -> Term    -- names to Bruijn
+removeNames (Lam.Var name) gamma = 
+  Var index where (Just index) = elemIndex name gamma     -- index
+removeNames (Lam.Abs name t) gamma = 
+  Abs $ removeNames t (name : gamma)                      -- recursively converting t into index
+removeNames (Lam.App t1 t2) gamma = 
+  App (removeNames t1 gamma) (removeNames t2 gamma)       -- recursively converting t1 & t2 into index
+
 restoreNames :: Term -> Gamma -> Lam.Term   -- Bruijn to names
+restoreNames (Var index) gamma = 
+  Lam.Var $ gamma !! index                                -- name
+restoreNames (Abs t) gamma = 
+  Lam.Abs name (restoreNames t $ name : gamma)            -- recursively converting t into name
+  where name = head $ ['a' ..] \\ gamma                   -- select from ['a' ..] removing gamma
+restoreNames (App t1 t2) gamma =
+  Lam.App (restoreNames t1 gamma)(restoreNames t2 gamma)  -- recursively converting t1 & t2 into names 
