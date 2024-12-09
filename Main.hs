d40a056dcfac64d69ce001ded1d91f534e3af1e0
W <jonathan.wnog@gmail.com>
2024-12-09 19:12:05 -0300

    evaluating input

--- Main.hs
+++ Main.hs
@@ -1,15 +1,19 @@
 module Main where
 
-import DeBruijn ()
+import DeBruijn ( removeNames, restoreNames )
 import Lambda ( LTerm (eval) )
 import qualified Lambda as Lam
 import Parser ( parserlamb, lexer )
+
+import Control.Monad.Writer (runWriter)
 
 interpret :: String -> IO ()
 interpret input = do
   let parsed = parserlamb $ lexer input -- syntax and lexical analysis
   let gamma = Lam.freeVars parsed       -- free vars from parsed
 
+  let h = snd $ runWriter $ eval $ removeNames parsed gamma
+  mapM_ (print . flip restoreNames gamma) h
 
 main :: IO ()
 main = do
