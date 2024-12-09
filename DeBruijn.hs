1f7b0a4a99fb932a89390b9ec137e745ec4f6562
W <jonathan.wnog@gmail.com>
2024-12-09 15:44:58 -0300

    shift and subs

--- DeBruijn.hs
+++ DeBruijn.hs
@@ -16,12 +16,7 @@ instance Show Term where
   show :: Term -> String
   show (Var index) = show index
   show (Abs t) = "(lam . " ++ show t ++ ")"
-  show (App s t) = "(" ++ show s ++ " " ++ show t ++ ")"
-
-isValue :: Term -> Bool
-isValue (Var _) = True
-isValue (Abs _) = True  
-isValue _ = False   -- aplication isnt value
+  show (App t1 t2) = "(" ++ show t1 ++ " " ++ show t2 ++ ")"
 
 removeNames :: Lam.Term -> Gamma -> Term    -- names to Bruijn
 removeNames (Lam.Var name) gamma = 
@@ -39,3 +34,20 @@ restoreNames (Abs t) gamma =
   where name = head $ ['a' ..] \\ gamma                   -- select from ['a' ..] removing gamma
 restoreNames (App t1 t2) gamma =
   Lam.App (restoreNames t1 gamma)(restoreNames t2 gamma)  -- recursively converting t1 & t2 into names 
+
+isValue :: Term -> Bool
+isValue (Var _) = True
+isValue (Abs _) = True  
+isValue _ = False   -- aplication isnt value
+
+shift :: Term -> Int -> Int -> Term
+shift (Var index) d c = Var $ if index < c then index else (index + d)      -- index
+shift (Abs t) d c = Abs $ (shift t d (c+1))                                 -- abstraction
+shift (App t1 t2) d c = App (shift t1 d c) (shift t2 d c)                   -- aplication
+
+subs :: Term -> Int -> Term -> Term
+subs (Var index) j s = if index == j then s else (Var index)                -- index
+subs (Abs t) j s = Abs $ subs t (j+1) (shift s 1 0)                         -- abstraction
+subs (App t1 t2) j s = App (subs t1 j s) (subs t2 j s)                      -- aplication
+
+betaReduction :: 
